1417587c47cbd2f9625dd7b0dc2c34077d54f0ad
Reckordp <rmuhammad134@gmail.com>
2022-09-06 17:44:21 +0700

    use variable

--- app/src/main/java/com/reckordp/berkirimfile/JukirServer.java
+++ app/src/main/java/com/reckordp/berkirimfile/JukirServer.java
@@ -2,7 +2,6 @@ package com.reckordp.berkirimfile;
 
 import java.io.IOException;
 import java.net.InetAddress;
-import java.net.NetworkInterface;
 import java.net.Socket;
 
 public class JukirServer {
@@ -15,13 +14,14 @@ public class JukirServer {
     }
 
     public boolean isServer() throws IOException {
+        boolean portNormal = false;
         Socket koneksi;
         RuangTunggu.PenghantarServer server;
         if (ipAddr.isReachable(1000)) {
             koneksi = new Socket(ipAddr, RuangTunggu.PenghantarServer.SOCKET_PORT_SERVER);
             server = new RuangTunggu.PenghantarServer(koneksi);
-            return server.ujiPort();
+            portNormal = server.ujiPort();
         }
-        return false;
+        return portNormal;
     }
 }
